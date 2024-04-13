6075529eeb03cc1ef219748e1532ca00a1e529ca
SultanInStem <sultaninstem004@gmail.com>
2024-04-14 07:08:23 +0800

    refresh token

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,4 +1,5 @@
-import express from "express"; 
+import express from "express";
+import refreshToken from "../controllers/Auth/refreshToken.js"; 
 import login from "../controllers/Auth/Login.js";
 import createAdmin from "../controllers/Auth/createAdmin.js"; 
 const router = express.Router(); 
@@ -6,5 +7,5 @@ const router = express.Router();
 
 router.post("/login", login); 
 router.post("/create-admin", createAdmin); 
-
+router.post('/refreshtoken', refreshToken);
 export default router; 
--- utils/verifyRefreshToken.js
+++ utils/verifyRefreshToken.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 const verifyRefreshToken = (token) => {
-    return jwt.verify(token, process.env.JWT_ACCESS_KEY); 
+    return jwt.verify(token, process.env.JWT_REFRESH_KEY); 
 }
 export default verifyRefreshToken;
