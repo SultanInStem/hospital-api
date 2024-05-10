27615b7ca9cac5f6690cfb19043614e8a39d43f4
SultanInStem <sultaninstem004@gmail.com>
2024-05-10 12:49:12 +0800

    public endpoints

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,27 +1,11 @@
 import express from "express"; 
 const router = express.Router(); 
-import createDoctor from '../controllers/Admin/doctorManagement/createDoctor.js'; 
-import getDoctors from '../controllers/Admin/doctorManagement/getDoctors.js'; 
-import deleteDoctor from '../controllers/Admin/doctorManagement/deleteDoctor.js'; 
 import createPatient from '../controllers/Admin/patientManagement/createPatient.js'; 
-import updateDoctor from "../controllers/Admin/doctorManagement/updateDoctor.js";
-import createService from "../controllers/Admin/serviceManagement/createService.js";
-import deleteService from "../controllers/Admin/serviceManagement/deleteService.js";
-import getServices from "../controllers/Admin/serviceManagement/getServices.js";
-import updateService from "../controllers/Admin/serviceManagement/updateService.js";
 import updatePatient from "../controllers/Admin/patientManagement/updatePatient.js";
 import deletePatient from "../controllers/Admin/patientManagement/deletePatient.js";
 import createMedicalRecord from "../controllers/Admin/patientManagement/createMedicalRecord.js";
 import makeRefund from "../controllers/Admin/finanaceManagment/makeRefund.js";
 import getPayments from "../controllers/Admin/finanaceManagment/getPayments.js";
-// Docs
-// router.post('/doctors/create', createDoctor);
-router.get('/doctors', getDoctors);  
-// router.delete('/doctors/:id', deleteDoctor); 
-// router.patch("/doctors/:id", updateDoctor);
-//-----------
-
-
 
 
 // Patients 
@@ -32,14 +16,6 @@ router.patch("/patients/:id", updatePatient);
 
 // Medical records 
 router.post("/medicalrecords/create", createMedicalRecord);
-
-// Services 
-router.get('/services', getServices); 
-// router.post('/services/create', createService);
-// router.delete("/services/:id", deleteService); 
-// router.patch("/services/:id", updateService);
-// --------
-
 
 // Finance 
 router.post("/finance/refund", makeRefund); 
--- routes/PublicRouter.js
+++ routes/PublicRouter.js
@@ -1,10 +1,24 @@
 import express from "express";
-import getAllPatients from "../controllers/Common/getAllPatients.js"; 
-import patientSearch from "../controllers/Common/patientSearch.js"; 
-import getMedicalRecords from "../controllers/Common/getMedicalRecords.js";
+import getAllPatients from "../controllers/Public/getAllPatients.js"; 
+import patientSearch from "../controllers/Public/patientSearch.js"; 
+import getMedicalRecords from "../controllers/Public/getMedicalRecords.js";
+import getServices from "../controllers/Public/getServices.js";
+import getDoctors from "../controllers/Public/getDoctors.js"; 
 const router = express.Router(); 
-
+// Patients 
 router.get('/patients', getAllPatients);
 router.get("/patients/search", patientSearch);
+//
+
+// Services 
+router.get('/services', getServices); 
+//
+
+// Medical records
 router.get("/medicalrecords", getMedicalRecords); // ":id" is of the patient
+//
+
+// Doctors 
+router.get('/doctors', getDoctors); 
+//
 export default router; 
