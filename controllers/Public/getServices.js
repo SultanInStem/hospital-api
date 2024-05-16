7ac3e50eaf8fc50da7d4e70f97d1e59da957a4d0
rick-n-shawty <sultanhoodie@gmail.com>
2024-05-17 07:39:42 +0800

    working getServices

--- controllers/Public/getServices.js
+++ controllers/Public/getServices.js
@@ -1,10 +1,25 @@
 import Service from "../../db/models/Service.js";
 import { StatusCodes } from "http-status-codes";
-
+import joi from "joi"; 
+import validateData from "../../utils/validateData.js";
+const joiSchema = joi.object({
+    price: joi.number().min(0).allow(0).optional(),
+    title: joi.string().optional(),
+    size: joi.number().min(0).allow(-1).required(), 
+})
 const getServices = async(req, res, next) => {
     try{
-        
-        return res.status(StatusCodes.OK).json({success: true})
+        const query = await validateData(joiSchema,req.query);  
+        const newQuery = {...query}; 
+        delete newQuery['size']; 
+        newQuery['isAvailable'] = true;
+        let services = []
+        if(query['size'] === -1){
+            services = await Service.find(newQuery, {createdAt: 0, updatedAt: 0, description: 0}); 
+        }else{
+            services = await Service.find(newQuery, {createdAt: 0, updatedAt: 0, description: 0}).limit(query['size']);
+        }
+        return res.status(StatusCodes.OK).json({success: true,services})
     }catch(err){
         return next(err); 
     }
