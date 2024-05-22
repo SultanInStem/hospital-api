3bbe4b0381db1963ec75afb7515e54c4b793c693
SultanInStem <sultaninstem004@gmail.com>
2024-05-22 13:21:51 +0800

    fixed queue function

--- controllers/Doctor/queueFunctionality/getPatientsQueue.js
+++ controllers/Doctor/queueFunctionality/getPatientsQueue.js
@@ -1,4 +1,3 @@
-import joi from "joi"; 
 import { StatusCodes } from "http-status-codes";
 import Service from "../../../db/models/Service.js"; 
 import PatientMedicalRecord from "../../../db/models/PatientMedicalRecords.js"; 
@@ -9,32 +8,19 @@ const getPatientsQueue = async(req, res, next) => {
         const doctorId = req.userId; 
         const services = await Service.find({ providedBy: doctorId, isAvailable: true });
         const netQueue = [];
-        const patientRecordProjection = {
-            updatedAt: 0,
-            paymentRecord: 0,
-        };
         for(let i = 0; i < services.length; i++){
             const queue = services[i].currentQueue; 
             if(queue.length < 1) continue; 
-            const firstPatient = await PatientMedicalRecord.findById(queue[0],patientRecordProjection); 
-            if(!firstPatient) continue;
-            firstPatient['service'] = services[i].title; 
-            queue[0] = firstPatient;
-            netQueue.push({time: firstPatient.createdAt, queue, serviceTitle: services[i].title});
-        }
-        const sortedQueueIds = _.sortBy(netQueue,'time'); 
-        const sortedPatients = []; 
-        for(let i = 0; i < sortedQueueIds.length; i++){
-            const queueIds = sortedQueueIds[i]['queue'];
-            const serviceTitle = sortedQueueIds[i]['serviceTitle'];
-            sortedPatients.push(queueIds[0]); 
-            for(let j = 1; j < queueIds.length; j++){
-                const patientRecord = await PatientMedicalRecord.findById(queueIds[j], patientRecordProjection); 
-                patientRecord['service'] = serviceTitle;
-                sortedPatients.push(patientRecord);
+            for(let j = 0; j < queue.length; j++){
+                const patientRecord = await PatientMedicalRecord.findById(queue[j], {paymentRecord: 0, updatedAt: 0, __v: 0});  
+                if(!patientRecord) continue; 
+                const recordObj = patientRecord.toObject(); 
+                recordObj['serviceTitle'] = services[i].title; 
+                netQueue.push(recordObj);   
             }
         }
-        return res.status(StatusCodes.OK).json({success: true, queue: sortedPatients}); 
+        const sortedQueue = _.sortBy(netQueue, 'createdAt');
+        return res.status(StatusCodes.OK).json({success: true, queue: sortedQueue}); 
     }catch(err){
         return next(err); 
     }
