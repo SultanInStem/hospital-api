a57ef9e3a6b3ef1b28c82305ee657fba3cfa0ec7
rick-n-shawty <sultanhoodie@gmail.com>
2024-05-22 11:45:36 +0800

    updated package funcs

--- db/models/MedPackage.js
+++ db/models/MedPackage.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const PackageSchema = new mongoose.Schema({
+    title: {
+        type: String, 
+        required: true
+    },
     price: {
         type: Number,
         required: true 
