27ade49c397fc1bf9453ea8a69df1018c0d4cb50
SultanInStem <sultaninstem004@gmail.com>
2024-06-05 13:05:19 +0800

    major

--- cleanupFunctions/cleanPatients.js
+++ cleanupFunctions/cleanPatients.js
@@ -1,9 +1,21 @@
 import Patient from "../db/models/Patient.js";
+const TIME_INTERVAL = Number(process.env.DB_CLEANUP_INTERVAL); 
 
 const cleanPatients = async() => {
     try{
-        // await Patient.deleteMany({}, );
-        return {success: true}; 
+        if(isNaN(TIME_INTERVAL)){
+            return {success: false, err: 'time interval is not a number!'};
+        }
+        const currentUnix = new Date().getTime(); 
+        const allPatients = await Patient.find();
+        let count = 0;
+        for(const patient of allPatients){
+            if(currentUnix - patient.lastSeen > TIME_INTERVAL){
+                await Patient.findByIdAndDelete(patient._id);
+                count++; 
+            }
+        }
+        return { success: true, deletedPatients: count }; 
     }catch(err){
         return {success: false, err: err}; 
     }
