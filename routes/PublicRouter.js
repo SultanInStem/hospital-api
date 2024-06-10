d83d8d6a77a82d21de417be3fcbea45b56dd40ab
SultanInStem <sultaninstem004@gmail.com>
2024-06-10 14:26:32 +0800

    get single service

--- routes/PublicRouter.js
+++ routes/PublicRouter.js
@@ -2,12 +2,13 @@ import express from "express";
 import getAllPatients from "../controllers/Public/Patients/getAllPatients.js"; 
 import searchPatients from "../controllers/Public/Patients/searchPatients.js";
 import getMedicalRecords from "../controllers/Public/MedicalRecords/getMedicalRecords.js";
-import getServices from "../controllers/Public/getServices.js";
 import getDoctors from "../controllers/Public/getDoctors.js"; 
 import getMedPackages from "../controllers/Public/getMedPackages.js";
 import getPaymnets from "../controllers/Public/getPayments.js";
 import getSinglePatient from "../controllers/Public/Patients/getSinglePatient.js";
 import getSingleRecord from "../controllers/Public/MedicalRecords/getSingleMedRecord.js";
+import getServices from "../controllers/Public/Services/getServices.js";
+import getSingleService from "../controllers/Public/Services/getSingleService.js";
 const router = express.Router(); 
 // Patients 
 router.get('/patients', getAllPatients);
@@ -17,6 +18,7 @@ router.get("/patients/single/:id", getSinglePatient);
 
 // Services 
 router.get('/services', getServices); 
+router.get("/services/single/:id", getSingleService); 
 //
 
 // Medical records
