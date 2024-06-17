593b9cc149a27db5a49958a332f94e0441b8db33
rick-n-shawty <sultanhoodie@gmail.com>
2024-06-17 14:22:17 +0800

    minor

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 export const mongoIdLength = 24;
 export const bonusPercentage = 0.01; 
-export const phonePattern = "/^\+\d{5}-\d{3}-\d{2}-\d{2}$/"; 
+export const phonePattern = /^\+\d{5}-\d{3}-\d{2}-\d{2}$/; 
