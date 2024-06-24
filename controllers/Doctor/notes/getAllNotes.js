10a8cf009e5d33cce6791ebcee00d2f8c31654fc
SultanInStem <sultaninstem004@gmail.com>
2024-06-24 13:21:03 +0800

    notes GET

---
+++ controllers/Doctor/notes/getAllNotes.js
@@ -0,0 +1,16 @@
+import { StatusCodes } from "http-status-codes";
+import Note from "../../../db/models/Note.js";
+
+
+
+const getAllNotes = async(req, res, next) => {
+    try{
+        const { id } = req.params; // patient id 
+        const notes = await Note.find({ patientId: id });
+        return res.status(StatusCodes.OK).json({success: true, notes});
+    }catch(err){
+        return next(err); 
+    }
+}
+
+export default getAllNotes; 
